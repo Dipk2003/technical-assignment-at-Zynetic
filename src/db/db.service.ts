c2b336fca3f9a93890fc179e0109e35c9968667c
ultimatedev03 <ultimatedev2025@gmail.com>
2026-02-10 00:24:28 +0530

    fix pg query typings

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -1,12 +1,15 @@
 import { Inject, Injectable, OnApplicationShutdown } from '@nestjs/common';
-import { Pool, QueryResult } from 'pg';
+import { Pool, QueryResult, QueryResultRow } from 'pg';
 import { PG_POOL } from './db.constants';
 
 @Injectable()
 export class DbService implements OnApplicationShutdown {
   constructor(@Inject(PG_POOL) private readonly pool: Pool) {}
 
-  query<T = any>(text: string, params?: any[]): Promise<QueryResult<T>> {
+  query<T extends QueryResultRow = any>(
+    text: string,
+    params?: any[],
+  ): Promise<QueryResult<T>> {
     return this.pool.query(text, params);
   }
 
